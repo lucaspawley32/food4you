dcf00447641af8c011d4cca0034c727ba5dde288
Lucas Pawley <lucaspawley@cmail.carleton.ca>
2019-02-16 13:13:45 -0500

    push

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          title="click me"
+          color="#249300"
+          accessibilityLabel="Learn more about this green button"
+        />
       </View>
     );
   }
