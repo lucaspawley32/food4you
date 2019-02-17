7d4cc1e76dd652db105c3ef64ceee832933d0f2b
Tom <tom.trinh.nguyen@gmail.com>
2019-02-16 21:06:17 -0500

    Functionality added

--- AddFood.js
+++ AddFood.js
@@ -7,6 +7,10 @@ let screen = Dimensions.get('window');
 export default class AddFood extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name:'',
+      description:''
+    }
   }
 
   showAddFood = () => {
@@ -18,20 +22,46 @@ export default class AddFood extends React.Component {
       <Modal
         ref={'myAddFood'}
         style={{
-          justifyContent: 'center',
-          width: screen.width - 10,
           height: 200
         }}
         position='center'
-        onClose={() => {
-          alert("food added");
+        onClosed={() => {
+          alert("food added: \n" + this.state.name + "\n" + this.state.description);
         }}
       >
-        <Text>Please add info about free food!</Text>
+        <Text
+          style={{
+            textAlign: 'center',
+            marginTop: 20
+          }}
+        >Please add info about free food!</Text>
+
         <TextInput
           style={{
-            height: 40
+            height: 40,
+            borderBottomColor: 'gray',
+            borderBottomWidth: 1,
+            marginTop: 20,
+            marginLeft: 40,
+            marginRight: 40
           }}
+          onChangeText={(name)=>this.setState({name:name})}
+          placeholder="Enter the name of your place"
+          value={this.state.name}
+        />
+
+        <TextInput
+          style={{
+            height: 40,
+            borderBottomColor: 'gray',
+            borderBottomWidth: 1,
+            marginTop: 20,
+            marginLeft: 40,
+            marginRight: 40
+          }}
+          onChangeText={(description)=>this.setState({description:description})}
+          placeholder="Describe your food"
+          value={this.state.description}
         />
       </Modal>
     );
